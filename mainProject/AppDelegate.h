6b2fe4642362a8aaaac67872607087ba53069c84
Alan <zanma@zanmas-Mac-mini.local>
2019-04-02 17:33:30 +0800

    first commit

---
+++ mainProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mainProject
+//
+//  Created by Alan on 2019/4/2.
+//  Copyright © 2019 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mainProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mainProject
+//
+//  Created by Alan on 2019/4/2.
+//  Copyright © 2019 Alan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
